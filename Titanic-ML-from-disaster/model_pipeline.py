c6dd1f72e788e080d452a19b65a3a19087926c43
capac <capac@users.noreply.github.com>
2019-08-15 13:01:10 +0100

    Consolidated scikit-learn calls into module

--- Titanic-ML-from-disaster/model_pipeline.py
+++ Titanic-ML-from-disaster/model_pipeline.py
@@ -9,13 +9,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
-from sklearn.externals import joblib
+from sklearn.metrics import mean_squared_error, confusion_matrix, classification_report, roc_auc_score, roc_curve
 from time import time
+import joblib
+import numpy as np
+import matplotlib.pyplot as plt
 
 class ModelPipeline():
-    def __init__(self, train_df, test_df, cat_attrib, num_attrib):
+    """Class for creating model pipeline that uses the Simpleimputer class (with the 'medium' strategy) for numerical features and the OneHotEncoder class for categorical features."""
+
+    def __init__(self, train_df, cat_attrib, num_attrib):
         self.train_df = train_df
-        self.test_df = test_df
         self.cat_attrib = cat_attrib
         self.num_attrib = num_attrib
 
@@ -32,6 +36,8 @@ class ModelPipeline():
         return X_train, X_test, y_train, y_test
 
 class BinaryModelTraining():
+    """Class for model fitting according to the dictionary of model parameters used as argument"""
+
     def __init__(self, X_train, X_test, y_train, y_test):
         self.X_train = X_train
         self.X_test = X_test
@@ -50,5 +56,46 @@ class BinaryModelTraining():
             y_pred_proba_results.append(y_pred_proba)
             joblib.dump(model, '_'.join(name.lower().split(' '))+'.pkl')
             print(f'Accuracy of the {name.lower()} on test set: {model.score(self.X_test, self.y_test):.4f}')
-        print(f'Time elapsed: {(time() - t0):.2f} seconds')
+        print(f'Time elapsed for training: {(time() - t0):.2f} seconds\n')
         return y_pred_results, y_pred_proba_results
+
+class BinaryClassMetrics():
+    """Class for the calculation of the mean squared error, confusion matrix, precision, recall, F-measure, support and ROC area-under-the-curve score"""
+
+    def __init__(self, model_dict, y_pred_results, y_pred_proba_results):
+        self.model_dict = model_dict
+        self.y_pred_results = y_pred_results
+        self.y_pred_proba_results = y_pred_proba_results
+
+    def compute_metrics(self, y_test):
+        """Requires the y_test array as a parameter"""
+
+        for (name, _), y_pred in zip(self.model_dict.items(), self.y_pred_results):
+            mse = mean_squared_error(y_test, y_pred)
+            print(f'Root mean square error for the {name.lower():s} model: {np.sqrt(mse):.3f}')
+        print('\n', end='')
+
+        for (name, _), y_pred in zip(self.model_dict.items(), self.y_pred_results):
+            cm = confusion_matrix(y_test, y_pred)
+            print(f'Confusion matrix for the {name.lower()} model: \n{cm}\n')
+
+        for (name, _), y_pred in zip(self.model_dict.items(), self.y_pred_results):
+            class_report = classification_report(y_test, y_pred)
+            print(f'Precision, recall, F-measure and support for the {name.lower()} model: \n{class_report}\n')
+
+    def compute_plots(self, y_test):
+        """Requires the y_test array as a parameter"""
+
+        for (name, _), y_pred, y_pred_proba in zip(self.model_dict.items(), self.y_pred_results, self.y_pred_proba_results):
+            model_roc_auc = roc_auc_score(y_test, y_pred)
+            fpr, tpr, _ = roc_curve(y_test, y_pred_proba[:,1])
+            _, axes = plt.subplots(figsize=(10, 8))
+            axes.plot(fpr, tpr, marker='.', ms=6, label='Model: {0:s}, Regression (area = {1:.3f})'.format(name.lower(), model_roc_auc))
+            axes.plot([0, 1], [0, 1],'r--')
+            axes.set_xlim([0.0, 1.0])
+            axes.set_ylim([0.0, 1.05])
+            axes.set_xlabel('False Positive Rate')
+            axes.set_ylabel('True Positive Rate')
+            axes.set_title('Receiver operating characteristic for the {0:s} model'.format(name.lower()))
+            axes.legend(loc="lower right")
+            plt.grid(True, linestyle='--')
